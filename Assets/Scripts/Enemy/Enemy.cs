9fbe8dd26d4be08817eedbe4af60d0b1e52ce548
Domenico Gagliano <domenico.gagliano1973+github@gmail.com>
2020-06-02 12:32:21 +0100

    Set the abstract Enemy class to be used by any type of enemy for movement

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,10 +9,58 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected int gems;     //protected = only object that ihnerit can modify it.
 
     [SerializeField] protected Transform pointA, pointB; //waypoints
-    public virtual void Attack()  //may or maynot be further implemented in the child class
+
+    protected Vector3 currentTarget;
+    protected Animator anim;
+    protected SpriteRenderer sprite;
+
+    private void Start()
     {
-        Debug.Log("My name is: " + this.gameObject.transform.name);
+        Init();
+    }
+    //Initalization function
+    public virtual void Init()
+    {
+        anim = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    public virtual void Movement()
+    {
+        //face the Enemy sprite in the correct direction
+        if (currentTarget == pointA.position)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+
+        //set the target waypoint
+        if (this.transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            anim.SetTrigger("Idle");
+        }
+        else if (this.transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            anim.SetTrigger("Idle");
+        }
+
+        //move the Enemy
+        this.transform.position = Vector3.MoveTowards(this.transform.position, currentTarget, speed * Time.deltaTime);
     }
 
-    public abstract void Update(); //must be implemented in the child class
+    public virtual void Update()
+    {
+        //if idle animation  in playing
+        //do nothing
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
 }
--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,53 +4,6 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    private Vector3 _currentTarget;
-    private Animator _anim;
-    private SpriteRenderer _mossSprite;
-    private void Start()
-    {
-        _anim = GetComponentInChildren<Animator>();
-        _mossSprite = GetComponentInChildren<SpriteRenderer>();
-    }
 
-    public override void Update()
-    {
-        //if idle animation  in playing
-        //do nothing
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            return;
-        }
-        
-        Movement();
 
-    }
-
-    private void Movement()
-    {
-        //face the Moss Giant sprite in the correct direction
-        if (_currentTarget == pointA.position)
-        {
-            _mossSprite.flipX = true;
-        }
-        else
-        {
-            _mossSprite.flipX = false;
-        }
-
-        //set the target waypoint
-        if (this.transform.position == pointA.position)
-        {  
-            _currentTarget = pointB.position;
-            _anim.SetTrigger("Idle");
-        }
-        else if (this.transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-            _anim.SetTrigger("Idle");
-        }
-
-        //move the moss giant
-        this.transform.position = Vector3.MoveTowards(this.transform.position, _currentTarget, speed * Time.deltaTime);
-    }
 }
